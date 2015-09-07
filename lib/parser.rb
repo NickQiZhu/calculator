16d8406de69f071cb474f08da9829f8526ec6e87
Nick Zhu <nick.qi.zhu@gmail.com>
2015-09-07 05:27:03 -0400

    [nz] implemented order of operations

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,24 +1,37 @@
 class Parser
-  def parse(tokens)
-    stack = []
+  ORDER_OF_OPS = {:+ => 1, :- => 1, :* => 0, :/ => 0}
 
-    tokens.each do |token|
-      if form_operation?(stack, token)
-        operator = stack.pop
-        left = stack.pop
-        right = token
-        stack.push [operator, left, right]
-      else
-        stack.push token
-      end
+  def parse(tokens)
+    stack = tokens
+
+    op_orders.each do |op_order|
+      stack = parse_in_order(stack, op_order)
     end
 
     stack.pop
   end
 
+  def parse_in_order(tokens, op_order)
+    stack = []
+
+    tokens.each do |token|
+      if operation_available?(stack, token)
+        handle_operation(op_order, stack, token)
+      else
+        stack.push token
+      end
+    end
+
+    stack
+  end
+
   private
 
-  def form_operation?(stack, token)
+  def op_orders
+    ORDER_OF_OPS.values.uniq.sort
+  end
+
+  def operation_available?(stack, token)
     operand?(token) && !first_token?(stack)
   end
 
@@ -33,4 +46,29 @@ class Parser
   def first_token?(stack)
     stack.empty?
   end
+
+  def handle_operation(op_order, stack, token)
+    operator = stack.pop
+    if matching_order?(op_order, operator)
+      form_operation(operator, stack, token)
+    else
+      skip_operation(operator, stack, token)
+    end
+  end
+
+  def matching_order?(op_order, operator)
+    ORDER_OF_OPS[operator] == op_order
+  end
+
+  def form_operation(operator, stack, token)
+    left = stack.pop
+    right = token
+    stack.push [operator, left, right]
+  end
+
+  def skip_operation(operator, stack, token)
+    stack.push operator
+    stack.push token
+  end
+
 end
